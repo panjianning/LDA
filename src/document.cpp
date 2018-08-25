4e4678e21e0e9d18d7967775cb2ce3b45cfab359
Jianning Pan <wspanjiannning@gmail.com>
2018-08-25 16:45:23 +0800

    Update document.cpp

--- src/document.cpp
+++ src/document.cpp
@@ -19,16 +19,17 @@ void LDADoc::add_token(const Token& token) {
    _topic_sum[token.topic]++;
 }
   
-void LDADoc::set_topic(int index, int new_topic) {
-    Log::CHECK(new_topic >= 0 && new_topic <_num_topics, 
-        "Topic %d out of range!", new_topic);
+void LDADoc::set_topic(int index, int topic) {
+
+    Log::CHECK(topic >= 0 && topic <_num_topics, 
+        "Topic %d out of range!", topic);
     int old_topic = _tokens[index].topic;
-    if (new_topic == old_topic) {
+    if (topic == old_topic) {
         return;
     }
-    _tokens[index].topic = new_topic;
+    _tokens[index].topic = topic;
     _topic_sum[old_topic]--;
-    _topic_sum[new_topic]++;
+    _topic_sum[topic]++;
 }
 
 }
